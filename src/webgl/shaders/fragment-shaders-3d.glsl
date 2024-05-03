976ae15fe2ceb52e5d1bb521557a3393f2deb9fa
yansans <66671259+yansans@users.noreply.github.com>
2024-05-03 18:47:32 +0700

    feat: add variable for directional lighting

--- src/webgl/shaders/fragment-shaders-3d.glsl
+++ src/webgl/shaders/fragment-shaders-3d.glsl
@@ -2,10 +2,20 @@ precision mediump float;
 
 // Passed in from the vertex shader.
 varying vec4 v_color;
+varying vec3 v_normal;
 
 // uniform vec4 u_colorMult;
 // uniform vec4 u_colorOffset;
 
+uniform vec3 u_reverseLightDirection;
+uniform vec4 u_color;
+
 void main() {
+   // vec3 normal = normalize(v_normal);
+
+   // float light = dot(normal, u_reverseLightDirection);
+
    gl_FragColor = v_color;
+
+   // gl_FragColor.rgb *= light;
 }
--- src/webgl/shaders/vertex-shaders-3d.glsl
+++ src/webgl/shaders/vertex-shaders-3d.glsl
@@ -2,6 +2,8 @@ attribute vec4 a_position;
 attribute vec4 a_color;
 
 uniform mat4 u_matrix;
+uniform mat4 u_worldViewProjection;
+uniform mat4 u_worldInverseTranspose;
 
 varying vec4 v_color;
 
@@ -11,4 +13,7 @@ void main() {
 
   // Pass the color to the fragment shader.
   v_color = a_color;
+
+  // Orient the normals and pass to the fragment shader
+  // v_normal = mat3(u_worldInverseTranspose) * a_normal;
 }
